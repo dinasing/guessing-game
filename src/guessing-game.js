4182f1f2bdee75a264961dfb66289f1a2650ccd7
Dina Singatulina <dina.sing1212@gmail.com>
2019-10-13 18:07:46 +0300

    100

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
+    constructor() {
+        this.result = 0;
+        this.start = 0;
+        this.end = 0;
+        this.middle;
+      }
+    
+      setRange(min, max) {
+          this.start = min;
+          this.end = max;
+      }
+    
+      guess() {
+        this.middle= Math.ceil((this.start + this.end) / 2);
+        this.result = this.middle;
+        return this.result;
+      }
+    
+      lower() {
+        this.end= this.middle;
+      }
+    
+      greater() {
+        this.start = this.middle;
+      }
+  }
 
 module.exports = GuessingGame;
